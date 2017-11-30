9945cdaf5f9de2cdb56ea3c3a2aac100519db5f4
Bruno Lima <bgcl@ic.ufal.br>
2017-11-30 16:09:36 -0200

    Using queues containing command str. TODO: Separate queue for command characters

--- main.c
+++ main.c
@@ -6,27 +6,65 @@
 #include "queue.h"
 #include "esp/uart.h"
 
-void sender_task(void *p)
+#define MAX_LINE_SIZE 81
+
+/*
+ * Queues used to task intercommunication
+ */
+static QueueHandle_t tsqueue;
+
+void receiver_task(void *pvParameters)
 {
+     char rx_str[81];
+    while(1) {
+        if( xQueueReceive(tsqueue, rx_str, 1000 / portTICK_PERIOD_MS) )
+                  printf("Received: %s\n", rx_str);
+    }
+}
+
+void keyboard_monitor_task(void *pvParameters)
+{
+    char ch;
+    char cmd[MAX_LINE_SIZE];   // Max string size is 80. Last char must be \n or \r
     int i = 0;
+    printf("\n\n\nWelcome to gpiomon. Type 'help<enter>' for, well, help\n");
+    printf("%% ");
+    fflush(stdout); // stdout is line buffered
     while(1) {
-        printf("Send %i to receiver task!!!\n", i);
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        if (read(0, (void*)&ch, 1)) { // 0 is stdin
+            printf("%c", ch);
+            fflush(stdout);
+            if (ch == '\n' || ch == '\r') {
+                cmd[i] = 0;
+                i = 0;
+                printf("\n");
+
+                // Allow handler to execute its
+//                receiver_handle((char*) cmd);   // Handle cmd line that was read
+                if ( xQueueSend(tsqueue, cmd, 300 / portTICK_PERIOD_MS) )
+                    printf("Sending command to queue!\n");
+
+                printf("%% ");
+                fflush(stdout);
+            } else {
+                if (i < sizeof(cmd)) cmd[i++] = ch;
+            }
+        } else {
+            printf("You will never see this print as read(...) is blocking\n");
+        }
+
     }
 }
-
-void receiver_task(void *p)
-{
-    int rx_int = 0;
-    while(1) {
-        printf("Received %i world!\n", rx_int);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
 
 int user_init( void )
 {
     uart_set_baud(0, 115200);
-    xTaskCreate(my_task, (signed char*) "my_task", 1024, NULL, 1, NULL);
+
+    /*
+     * We need a queue of the size of max command line
+     */
+    tsqueue = xQueueCreate(5, sizeof(char)*MAX_LINE_SIZE);
+    xTaskCreate(keyboard_monitor_task, (signed char*) "key_mon", 1024, NULL, 1, NULL);
+    xTaskCreate(receiver_task, (signed char*) "receiver_task", 1024, NULL, 1, NULL);
+
 }
